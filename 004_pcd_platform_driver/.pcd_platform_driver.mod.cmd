0a6285ad27f0f7c1af69f357b9205deec247d693
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2022-11-14 19:51:47 +0530

    To be Included later

--- 004_pcd_platform_driver/.pcd_platform_driver.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/reckon/workspace/ldd/custom_drivers/004_pcd_platform_driver/pcd_platform_driver.mod := { echo  /home/reckon/workspace/ldd/custom_drivers/004_pcd_platform_driver/pcd_platform_driver.o;  echo; } > /home/reckon/workspace/ldd/custom_drivers/004_pcd_platform_driver/pcd_platform_driver.mod
--- 004_pcd_platform_driver/pcd_platform_driver.mod
+++
@@ -1,2 +0,0 @@
-/home/reckon/workspace/ldd/custom_drivers/004_pcd_platform_driver/pcd_platform_driver.o
-
