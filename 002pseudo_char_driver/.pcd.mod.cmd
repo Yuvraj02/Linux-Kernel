f4ca549589d693a0a6f384bc9ddeebf11487cdda
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2022-11-14 19:55:17 +0530

    Batch Files Removed

--- 002pseudo_char_driver/.pcd.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/reckon/workspace/ldd/custom_drivers/002pseudo_char_driver/pcd.mod := { echo  /home/reckon/workspace/ldd/custom_drivers/002pseudo_char_driver/pcd.o;  echo; } > /home/reckon/workspace/ldd/custom_drivers/002pseudo_char_driver/pcd.mod
